275a652292189c07b2797a53316ebf45568a2ad8
DmitryLyakhivsky <dlyakhivskiy@gmail.com>
2024-02-18 13:08:04 +0200

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,16 @@
-
-
-
-
-
-
-
-
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        return (this.username = newName);
+    },
+    updatePlayTime(hours) {
+      return (this.playTime += hours);
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
 
 
 console.log(profile.getInfo()); // "Jacob has 300 active hours!"
