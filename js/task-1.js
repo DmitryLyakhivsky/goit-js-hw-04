3f46f7f9ab959de9cbbdb1862341e450db669307
DmitryLyakhivsky <dlyakhivskiy@gmail.com>
2024-02-18 12:58:23 +0200

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,10 @@
-
-
-
-
-
-
+function isEnoughCapacity(products, containerSize) {
+let totalProducts = 0;
+    for (const product in products) {
+        totalProducts += products[product];
+  }
+  return totalProducts <= containerSize;
+}
 
 
 
